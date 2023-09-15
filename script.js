9c0ef0d74296a277b53b4cde3a20b6d81c534e06
Kanak Gupta <129761215+kanak-gupta30@users.noreply.github.com>
2023-09-15 12:59:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const inputbox=document.getElementById('input-box');
-const listcontainer=document.getElementById('container')
+const inputbox = document.getElementById('input-box');
+const listcontainer = document.getElementById('container');
+const additem=document.getElementById('button')
+additem.addEventListener('click',function addelement() {
+    if (inputbox.value == '') { alert("you must write something."); }
+    else {
+        let li = document.createElement('li');
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        const span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = '';
+})
+listcontainer.addEventListener('click',(e)=>{
+    if(e.target.tagName ==="SPAN"){e.target.parentElement.remove();}
 
-
-function addelement(){
-if(inputbox.value==''){alert("you must write something.");}
-else{
-    let li=document.createElement("li");
-    li.innerHTML=inputbox.value;
-    listcontainer.appendChild(li);
-    let span=document.createElement("span");
-    span.innerHTML="\u00d7";
-    li.appendChild(span);
-}
-inputbox.value='';
-}
+})
